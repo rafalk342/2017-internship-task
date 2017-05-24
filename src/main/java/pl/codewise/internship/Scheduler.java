5dbdcc2b0f445afcaf3d92fd9dcf6cf0246a15cf
rafalk342 <rafalk342@gmail.com>
2017-05-24 11:32:29 +0200

    Handling delays

--- src/main/java/pl/codewise/internship/Scheduler.java
+++ src/main/java/pl/codewise/internship/Scheduler.java
@@ -5,6 +5,49 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Scheduler {
     private ConcurrentHashMap<Integer, Timer> callbacks = new ConcurrentHashMap<>();
     private Integer currentId = 0;
+    private boolean stopScheduler = false;
+    private Thread timerThread;
+
+    public Scheduler() {
+        this.timerThread = new Thread(this::checkDelays);
+        this.timerThread.start();
+    }
+
+    public void stopScheduler() {
+        this.stopScheduler = true;
+    }
+
+    private long minDelay() {
+        long min = Long.MAX_VALUE;
+        for (Timer t : callbacks.values()) {
+            min = Math.min(min, t.timeCall);
+        }
+        return min;
+    }
+
+    private void checkRunRemove() {
+        callbacks.entrySet().removeIf(entry -> {
+            Timer t = entry.getValue();
+            if (System.currentTimeMillis() >= t.timeCall) {
+                t.callback.run();
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void checkDelays() {
+        while (!(stopScheduler && callbacks.isEmpty())) {
+            checkRunRemove();
+            long timeWait = minDelay() - System.currentTimeMillis();
+            try {
+                synchronized (this) {
+                    wait(timeWait);
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+    }
 
     public synchronized Integer callbackSize() {
         return callbacks.size();
@@ -14,11 +57,13 @@ public class Scheduler {
         currentId++;
         Integer newId = currentId;
         callbacks.put(newId, new Timer(newId, callback, System.currentTimeMillis() + delay));
+        timerThread.interrupt();
         return currentId;
     }
 
     public synchronized void stop(Integer taskId) {
         callbacks.remove(taskId);
+        timerThread.interrupt();
     }
 
     class Timer {
@@ -32,4 +77,5 @@ public class Scheduler {
             this.timeCall = timeCall;
         }
     }
+
 }
--- src/test/java/pl/codewise/internship/SchedulerTest.java
+++ src/test/java/pl/codewise/internship/SchedulerTest.java
@@ -1,17 +1,34 @@
 package pl.codewise.internship;
 
 import org.junit.Test;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import static org.junit.Assert.*;
+
 public class SchedulerTest {
+    Scheduler scheduler = new Scheduler();
+
     @Test
     public void startStopTest() throws Exception {
-        Scheduler scheduler = new Scheduler();
-        Integer id1 =scheduler.start(()-> System.out.println("new task"), 1000);
-        Integer id2 =scheduler.start(()-> System.out.println("new task"), 1000);
+        Integer id1 = scheduler.start(() -> System.out.println("new task"), 1000);
+        Integer id2 = scheduler.start(() -> System.out.println("new task"), 1000);
         assertEquals(new Integer(2), scheduler.callbackSize());
         scheduler.stop(id1);
         assertEquals(new Integer(1), scheduler.callbackSize());
         scheduler.stop(id2);
         assertEquals(new Integer(0), scheduler.callbackSize());
+        scheduler.stopScheduler();
+    }
+
+    @Test
+    public void callbackTest() throws Exception {
+        scheduler.start(() -> System.out.println("Callback"), 500);
+        Integer id = scheduler.start(() -> System.out.println("Callback2"), 1500);
+        Thread.sleep(2000);
+        scheduler.stop(id);
+        Thread.sleep(1000);
+        scheduler.stopScheduler();
     }
 }
